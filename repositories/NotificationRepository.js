f756f76f44603b79b70cdb25cf9663c3c1454c26
zero0661 <ttom50349@gmail.com>
2026-09-19 14:07:04 +0300

    Keep notification topic lookup schema-compatible

--- repositories/NotificationRepository.js
+++ repositories/NotificationRepository.js
@@ -84,8 +84,6 @@ export default {
                 n.*,
                 actor.name AS actor_name,
                 t.title AS topic_title,
-                t.title_ru AS topic_title_ru,
-                t.title_en AS topic_title_en,
                 t.room AS topic_room
             FROM notifications n
             LEFT JOIN guests actor
